f6b3e64c9e219aa562830819d3ffc3d98c19a6df
Ariel Carvajal <arie.cbpro@gmail.com>
2020-10-25 03:53:15 -0300

    Separates components

--- src/components/snippet-detail.js
+++ src/components/snippet-detail.js
@@ -5,37 +5,14 @@ import Modal from './modal';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/vs.css';
 
-import { ClockIcon, FileIcon, HeartIcon, PackageDependenciesIcon } from '@primer/octicons-react'
+import SnippetDetailHeader from './snippet-detail-header';
 
 export default function SnippetDetail(props) {
     window.f = hljs;
     // window.f.highlightBlock(document.getElementById('hello'))
     return (
         <Modal>
-            <div className='flex items-center px-4 py-2 space-x-4'>
-                <img className='inline-block h-12 w-12 rounded-full text-white shadow-solid object-center object-cover' src='assets/img/box.png' alt='' />
-                <div>
-                    <h1 className='text-2xl'>{props.author} / {props.title}</h1>
-                    <div className='flex space-x-2 text-gray-600 mb-2'>
-                        <div className='space-x-1 place-self-center'>
-                            <ClockIcon size='small' />
-                            <span className='inline-block align-middle'>12 Minutes ago</span>
-                        </div>
-                        <div className='space-x-1'>
-                            <FileIcon size='small' />
-                            <span className='inline-block align-middle'>1 File</span>
-                        </div>
-                        <div className='space-x-1'>
-                            <PackageDependenciesIcon size='small' />
-                            <span className='inline-block align-middle'>0 unbox?</span>
-                        </div>
-                        <div className='space-x-1'>
-                            <HeartIcon size='small' />
-                            <span className='inline-block align-middle'>1 Lifes saved</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <SnippetDetailHeader />
             <div className='grid grid-cols-3'>
                 <div className='flex-1 items-center px-2 py-2 border border-gray-300'>
                     <span>Resources</span>
